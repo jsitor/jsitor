e39b5ec06bea6b72f0430d9e3eb86bfaec734051
ashvin777 <ashvin.suthar777@gmail.com>
2018-04-18 22:25:18 +0530

    fixed issue related to #1

--- src/components/editors/output/output.vue
+++ src/components/editors/output/output.vue
@@ -4,7 +4,9 @@
       <span v-show="expandSourceType !== 'output'" class="right icon-maximize-2" @click="$emit('onExpandClicked', 'output')"></span>
       <span v-show="expandSourceType === 'output'" class="right icon-minimize-2" @click="$emit('onShrinkClicked')"></span>
     </header>
-    <iframe id="iframe-output"></iframe>
+
+    <div id="iframe-container"></div>
+    <!-- <iframe id="iframe-output" v-if="iframeSource"></iframe> -->
   </div>
 </template>
 
@@ -15,22 +17,8 @@ export default {
   props: ["css", "js", "html", "expandSourceType"],
 
   mounted() {
-    this.window = document.getElementById(
-      "iframe-output"
-    ).contentWindow;
+    this.iframeContainer = document.getElementById('iframe-container');
   },
-
-  // watch: {
-  //   html() {
-  //     this.write();
-  //   },
-  //   css() {
-  //     this.write();
-  //   },
-  //   js() {
-  //     this.write();
-  //   }
-  // },
 
   created(){
     EventBus.$on('run', clickCount => {
@@ -40,21 +28,29 @@ export default {
 
   methods: {
     write(){
-      this.window.document.open();
-      let htmlSource = '';
-      htmlSource += `<style>${this.css || ''}<\/style>`;
-      htmlSource += this.html || '';
-      htmlSource += `<script>${this.js || ''}<\/script>`;
 
-      this.window.document.write(htmlSource);
-      this.window.document.close();
+      let iframeSource = `<iframe id="iframe-output"></iframe>`;
+      this.iframeContainer.innerHTML = iframeSource;
+
+      this.source = '';
+      this.source += `<style>${this.css || ''}<\/style>`;
+      this.source += this.html || '';
+      this.source += `<script>${this.js || ''}<\/script>`;
+
+      let window = document.getElementById(
+        "iframe-output"
+      ).contentWindow;
+      window.document.open();
+      window.document.write(this.source);
+      window.document.close();
+
     }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
 iframe {
   border: 0;
   height: 100%;
