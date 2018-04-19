346c12084ff62869d5c7f82a3e5b448f727b6fee
ashvin777 <ashvin.suthar777@gmail.com>
2018-04-19 23:48:20 +0530

    added resizable ui editors and minor stylefixes

---
+++ deply.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+
+if [ "$#" -ne 1 ]; then
+   echo "./deploy.sh 'commit message'"
+   exit 1
+fi
+
+npm run build
+rm -r ../jsitor.github.io/static
+rm -r ../jsitor.github.io/index.html
+
+cp -r ./dist/* ../jsitor.github.io
+
+git add .
+git commit -m "$1"
+git push -u origin master
