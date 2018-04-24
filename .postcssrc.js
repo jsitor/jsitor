179a73b38eaa4a03fee9a1bddef7d619547cd4be
ashvin777 <ashvin.suthar777@gmail.com>
2018-04-24 18:06:03 +0530

    added autprefix settings and change title of app

--- .postcssrc.js
+++ .postcssrc.js
@@ -5,6 +5,8 @@ module.exports = {
     "postcss-import": {},
     "postcss-url": {},
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
+    "autoprefixer": {
+      browsers: ['last 30 versions']
+    }
   }
 }
